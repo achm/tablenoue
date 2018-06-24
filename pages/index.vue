4ed382428ad22580d11d5b958e434e2aab144b6f
achm <achm.pppp@gmail.com>
2018-06-25 00:34:18 +0900

    modify index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
-<template>
-  <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <div class="cards">
-      <Card v-for="person in people" :key="person.id" :person="person"></Card>
-    </div>
-  </section>
+<template lang="pug">
+section
+  section.hero.is-primary.is-medium
+    .hero-head
+      nav.navbar
+        .container
+          .navbar-brand
+            a.navbar-item(href="/") tablenoue
+          .navbar-menu
+            .navbar-end
+              a.navbar-item.is-active Home
+              a.navbar-item Games
+              a.navbar-item Plays
+
+    .hero-body
+      .container.has-text-centered
+        h1.title tablenoue
+        h2.subtitle わいわい たのしもう ボードゲーム
 </template>
 
 <script lang="ts">
@@ -24,14 +35,6 @@ export default class extends Vue {
   @State people
 }
 </script>
-<style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
-    sans-serif;
-}
 
-.cards {
-  display: flex;
-  flex-wrap: wrap;
-}
+<style scoped>
 </style>
