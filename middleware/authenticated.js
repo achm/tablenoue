650139056938b3ef3952c3ff00865af27d84c231
achm <achm.pppp@gmail.com>
2018-07-06 01:21:27 +0900

    fix TypeError

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,5 +1,5 @@
 export default function ({ store, route, redirect }) {
-  if (!store.getters["auth/isAuthenticated"] && route.name.indexOf("auth") > 0) {
+  if (!store.getters["auth/isAuthenticated"] && route.name && route.name.indexOf("auth") > 0) {
     redirect('/login')
   }
   if (store.getters["auth/isAuthenticated"] && route.name === 'login') {
