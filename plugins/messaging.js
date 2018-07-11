ff3db4806289231b7205c67a8cb78e57992eadd2
achm <achm.pppp@gmail.com>
2018-07-12 00:25:09 +0900

    disable messaging if unavailable serviceWorker

--- plugins/messaging.js
+++ plugins/messaging.js
@@ -1,5 +1,5 @@
 import firebase from '~/plugins/firebase'
-if (!process.server) {
+if (!process.server && navigator.serviceWorker) {
   const messaging = firebase.messaging()
   messaging.onMessage((payload) => {
     alert(payload.notification.body)
