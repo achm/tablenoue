f2e24a4f0db6eb800ea54e6fd7088aa5db8e82bd
achm <achm.pppp@gmail.com>
2018-07-11 22:20:31 +0900

    fix logout error

--- plugins/auth-cookie.js
+++ plugins/auth-cookie.js
@@ -2,7 +2,12 @@ import firebase from '~/plugins/firebase.js'
 
 export default (context) => {
   firebase.auth().onIdTokenChanged(async (user) => {
-    const token = await user.getIdToken()
-    document.cookie = `__session=${token};max-age=${(token ? 3600 : 0)};`
+    if (user) {
+      const token = await user.getIdToken()
+      document.cookie = `__session=${token};max-age=${(token ? 3600 : 0)};`
+
+    } else {
+      document.cookie = '__session=;max-age=0;'
+    }
   })
 }
